69570c4978ce841abc84fc5acf8ffd26f066b186
Elmer Nocon <elmernocon@gmail.com>
2025-03-24 08:42:26 +0800

    refactor(app): update app

--- src/app/__main__.py
+++ src/app/__main__.py
@@ -1,32 +1,50 @@
-# Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+# Copyright (c) 2025 AccelByte Inc. All Rights Reserved.
 # This is licensed software from AccelByte Inc, for limitations
 # and restrictions contact your company contract manager.
 
 import asyncio
 import logging
+from logging import Logger
+from typing import List, Optional
 
-from app.proto.section_pb2_grpc import add_SectionServicer_to_server
+from environs import Env
 
-from accelbyte_grpc_plugin import App, AppGRPCInterceptorOpt, AppGRPCServiceOpt
-from accelbyte_grpc_plugin.interceptors.authorization import (
-    AuthorizationServerInterceptor,
+from accelbyte_py_sdk.core import (
+    AccelByteSDK,
+    DictConfigRepository,
+    InMemoryTokenRepository,
+    HttpxHttpClient,
 )
-from accelbyte_grpc_plugin.interceptors.logging import (
-    DebugLoggingServerInterceptor,
-)
-from accelbyte_grpc_plugin.interceptors.metrics import (
-    MetricsServerInterceptor,
-)
-from accelbyte_grpc_plugin.opts.grpc_health_checking import GRPCHealthCheckingOpt
-from accelbyte_grpc_plugin.opts.grpc_reflection import GRPCReflectionOpt
-from accelbyte_grpc_plugin.opts.prometheus import PrometheusOpt
-from accelbyte_grpc_plugin.opts.zipkin import ZipkinOpt
+from accelbyte_py_sdk.services import auth as auth_service
 
-from accelbyte_grpc_plugin.utils import create_env, instrument_sdk_http_client
+from accelbyte_grpc_plugin import (
+    App,
+    AppOpt,
+    AppGRPCInterceptorOpt,
+    AppGRPCServiceOpt,
+)
+from accelbyte_grpc_plugin.utils import instrument_sdk_http_client
 
-from app.services.section_service import AsyncSectionService
+from .proto.section_pb2_grpc import add_SectionServicer_to_server
+from .services.section_service import AsyncSectionService
+from .utils import create_env
 
 DEFAULT_APP_PORT: int = 6565
+
+DEFAULT_AB_BASE_URL: str = "https://test.accelbyte.io"
+DEFAULT_AB_NAMESPACE: str = "accelbyte"
+
+DEFAULT_ENABLE_HEALTH_CHECK: bool = True
+DEFAULT_ENABLE_PROMETHEUS: bool = True
+DEFAULT_ENABLE_REFLECTION: bool = True
+DEFAULT_ENABLE_ZIPKIN: bool = True
+
+DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ENABLED: bool = True
+DEFAULT_PLUGIN_GRPC_SERVER_AUTH_RESOURCE: Optional[str] = None
+DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ACTION: Optional[int] = None
+
+DEFAULT_PLUGIN_GRPC_SERVER_LOGGING_ENABLED: bool = False
+DEFAULT_PLUGIN_GRPC_SERVER_METRICS_ENABLED: bool = True
 
 
 async def main(**kwargs) -> None:
@@ -34,74 +52,126 @@ async def main(**kwargs) -> None:
 
     port: int = env.int("PORT", DEFAULT_APP_PORT)
 
-    opts = []
     logger = logging.getLogger("app")
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler())
 
-    with env.prefixed("AB_"):
-        base_url = env("BASE_URL", "https://test.accelbyte.io")
-        client_id = env("CLIENT_ID", None)
-        client_secret = env("CLIENT_SECRET", None)
-        namespace = env("NAMESPACE", "accelbyte")
+    config = DictConfigRepository(dict(env.dump()))
+    token = InMemoryTokenRepository()
+    http = HttpxHttpClient()
+    http.client.follow_redirects = True
 
-    with env.prefixed(prefix="ENABLE_"):
-        if env.bool("PROMETHEUS", True):
-            opts.append(PrometheusOpt())
-        if env.bool("HEALTH_CHECKING", True):
-            opts.append(GRPCHealthCheckingOpt())
-        if env.bool("REFLECTION", True):
-            opts.append(GRPCReflectionOpt())
-        if env.bool("ZIPKIN", True):
-            opts.append(ZipkinOpt())
+    sdk = AccelByteSDK()
+    sdk.initialize(
+        options={
+            "config": config,
+            "token": token,
+            "http": http,
+        }
+    )
 
-    with env.prefixed(prefix="PLUGIN_GRPC_SERVER_AUTH_"):
-        if env.bool("ENABLED", False):
-            from accelbyte_py_sdk import AccelByteSDK
-            from accelbyte_py_sdk.core import MyConfigRepository, InMemoryTokenRepository
-            from accelbyte_py_sdk.token_validation.caching import CachingTokenValidator
-            from accelbyte_py_sdk.services.auth import login_client, LoginClientTimer
+    instrument_sdk_http_client(sdk=sdk, logger=logger)
 
-            config = MyConfigRepository(base_url, client_id, client_secret, namespace)
-            token = InMemoryTokenRepository()
+    _, error = await auth_service.login_client_async(sdk=sdk)
+    if error:
+        raise Exception(str(error))
 
-            sdk = AccelByteSDK()
-            sdk.initialize(options={"config": config, "token": token})
+    sdk.timer = auth_service.LoginClientTimer(2880, repeats=-1, autostart=True, sdk=sdk)
 
-            instrument_sdk_http_client(sdk=sdk, logger=logger)
-
-            result, error = login_client(sdk=sdk)
-            if error:
-                raise Exception(str(error))
-
-            sdk.timer = LoginClientTimer(2880, repeats=-1, autostart=True, sdk=sdk)
-
-            opts.append(
-                AppGRPCInterceptorOpt(
-                    interceptor=AuthorizationServerInterceptor(
-                        namespace=namespace,
-                        token_validator=CachingTokenValidator(sdk=sdk),
-                    )
-                )
-            )
-
-    if env.bool("PLUGIN_GRPC_SERVER_LOGGING_ENABLED", False):
-        opts.append(AppGRPCInterceptorOpt(DebugLoggingServerInterceptor(logger)))
-
-    if env.bool("PLUGIN_GRPC_SERVER_METRICS_ENABLED", True):
-        opts.append(AppGRPCInterceptorOpt(MetricsServerInterceptor()))
-
-    # register Filter Service
-    opts.append(
+    options = create_options(sdk=sdk, env=env, logger=logger)
+    options.append(
         AppGRPCServiceOpt(
-            AsyncSectionService(logger),
-            AsyncSectionService.full_name,
-            add_SectionServicer_to_server,
+            service=AsyncSectionService(
+                sdk=sdk,
+                logger=logger,
+            ),
+            service_full_name=AsyncSectionService.full_name,
+            add_service_func=add_SectionServicer_to_server,
         )
     )
 
-    await App(port, env, opts=opts).run()
+    app = App(port=port, env=env, logger=logger, options=options)
+    await app.run()
+
+
+def create_options(sdk: AccelByteSDK, env: Env, logger: Logger) -> List[AppOpt]:
+    options: List[AppOpt] = []
+
+    with env.prefixed("AB_"):
+        namespace = env.str("NAMESPACE", DEFAULT_AB_NAMESPACE)
+
+    with env.prefixed("ENABLE_"):
+        if env.bool("HEALTH_CHECK", env.bool("HEALTH_CHECKING", DEFAULT_ENABLE_HEALTH_CHECK)):
+            from accelbyte_grpc_plugin.opts.grpc_health_checking import (
+                GRPCHealthCheckingOpt,
+            )
+
+            options.append(GRPCHealthCheckingOpt())
+        if env.bool("PROMETHEUS", DEFAULT_ENABLE_PROMETHEUS):
+            from accelbyte_grpc_plugin.opts.prometheus import (
+                PrometheusOpt
+            )
+
+            options.append(PrometheusOpt())
+        if env.bool("REFLECTION", DEFAULT_ENABLE_REFLECTION):
+            from accelbyte_grpc_plugin.opts.grpc_reflection import (
+                GRPCReflectionOpt,
+            )
+
+            options.append(GRPCReflectionOpt())
+        if env.bool("ZIPKIN", DEFAULT_ENABLE_ZIPKIN):
+            from accelbyte_grpc_plugin.opts.zipkin import (
+                ZipkinOpt
+            )
+
+            options.append(ZipkinOpt())
+
+    with env.prefixed("PLUGIN_GRPC_SERVER_"):
+        with env.prefixed("AUTH_"):
+            if env.bool("ENABLED", DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ENABLED):
+                from accelbyte_py_sdk.token_validation.caching import CachingTokenValidator
+                from accelbyte_grpc_plugin.interceptors.authorization import AuthorizationServerInterceptor
+
+                options.append(
+                    AppGRPCInterceptorOpt(
+                        interceptor=AuthorizationServerInterceptor(
+                            token_validator=CachingTokenValidator(sdk=sdk),
+                            resource=env.str(
+                                "RESOURCE", DEFAULT_PLUGIN_GRPC_SERVER_AUTH_RESOURCE
+                            ),
+                            action=env.int(
+                                "ACTION", DEFAULT_PLUGIN_GRPC_SERVER_AUTH_ACTION
+                            ),
+                            namespace=namespace,
+                        )
+                    )
+                )
+        if env.bool("LOGGING_ENABLED", DEFAULT_PLUGIN_GRPC_SERVER_LOGGING_ENABLED):
+            from accelbyte_grpc_plugin.interceptors.logging import (
+                DebugLoggingServerInterceptor,
+            )
+
+            options.append(
+                AppGRPCInterceptorOpt(
+                    interceptor=DebugLoggingServerInterceptor(logger=logger)
+                )
+            )
+
+        if env.bool("METRICS_ENABLED", DEFAULT_PLUGIN_GRPC_SERVER_METRICS_ENABLED):
+            from accelbyte_grpc_plugin.interceptors.metrics import (
+                MetricsServerInterceptor,
+            )
+
+            options.append(
+                AppGRPCInterceptorOpt(interceptor=MetricsServerInterceptor())
+            )
+
+    return options
+
+
+def run() -> None:
+    asyncio.run(main())
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    run()
--- src/app/services/section_service.py
+++ src/app/services/section_service.py
@@ -1,17 +1,18 @@
-# Copyright (c) 2023 AccelByte Inc. All Rights Reserved.
+# Copyright (c) 2025 AccelByte Inc. All Rights Reserved.
 # This is licensed software from AccelByte Inc, for limitations
 # and restrictions contact your company contract manager.
 
 from datetime import datetime
 import json
-from logging import Logger, getLogger
+from logging import Logger
 import math
 from typing import List, Optional
-import uuid
 
 from google.protobuf.json_format import MessageToDict
 
-from app.proto.section_pb2 import (
+from accelbyte_py_sdk import AccelByteSDK
+
+from ..proto.section_pb2 import (
     BackfillRequest,
     BackfilledItemObject,
     RotationItemObject,
@@ -21,33 +22,33 @@ from app.proto.section_pb2 import (
     SectionItemObject,
     DESCRIPTOR,
 )
-from app.proto.section_pb2_grpc import SectionServicer
+from ..proto.section_pb2_grpc import SectionServicer
+
 
 class AsyncSectionService(SectionServicer):
     full_name: str = DESCRIPTOR.services_by_name["Section"].full_name
-    upper_limit : float = float(24)
+    upper_limit: float = float(24)
 
-    def __init__(self, logger: Optional[Logger]) -> None:
-        self.logger = (
-            logger if logger is not None else getLogger(self.__class__.__name__)
-        )
+    def __init__(self, sdk: Optional[AccelByteSDK] = None, logger: Optional[Logger] = None) -> None:
+        self.sdk = sdk
+        self.logger = logger
 
-    async def GetRotationItems(self, request : GetRotationItemsRequest, context):
+    async def GetRotationItems(self, request: GetRotationItemsRequest, context):
         """*
         GetRotationItems: get current rotation items, this method will be called by rotation type is CUSTOM
         """
         self.log_payload(f'{self.GetRotationItems.__name__} request: %s', request)
-        items : List[SectionItemObject] = request.sectionObject.items
-        input_count : int = len(items)
-        current_point : float = float(datetime.now().hour)
-        selected_index : int = int(math.floor((input_count/self.upper_limit)*current_point))
-        selected_item : SectionItemObject = items[selected_index]
-        response_items : List[SectionItemObject] = [selected_item]
-        response : GetRotationItemsResponse = GetRotationItemsResponse(expiredAt=0, items=response_items)
+        items: List[SectionItemObject] = request.sectionObject.items
+        input_count: int = len(items)
+        current_point: float = float(datetime.now().hour)
+        selected_index: int = int(math.floor((input_count/self.upper_limit)*current_point))
+        selected_item: SectionItemObject = items[selected_index]
+        response_items: List[SectionItemObject] = [selected_item]
+        response: GetRotationItemsResponse = GetRotationItemsResponse(expiredAt=0, items=response_items)
         self.log_payload(f'{self.GetRotationItems.__name__} response: %s', response)
         return response
 
-    async def Backfill(self, request : BackfillRequest, context):
+    async def Backfill(self, request: BackfillRequest, context):
         """*
         Backfill method trigger condition:
         1. Rotation type is FIXED_PERIOD
@@ -55,21 +56,22 @@ class AsyncSectionService(SectionServicer):
         3. User already owned any one of current rotation items.
         """
         self.log_payload(f'{self.Backfill.__name__} request: %s', request)
-        new_items : List[BackfilledItemObject] = []
-        item : RotationItemObject
+        new_items: List[BackfilledItemObject] = []
+        item: RotationItemObject
         for item in request.items:
-             if item.owned:
-                 backfill_item_id = request.items[-1].itemId    # Use the last item in the request for backfill for demo purpose only
-                 new_item : BackfilledItemObject = BackfilledItemObject(itemId=backfill_item_id, index=item.index)
-                 new_items.append(new_item)
-        response : BackfillResponse = BackfillResponse(backfilledItems=new_items)
+            if item.owned:
+                # Use the last item in the request for backfill for demo purpose only
+                backfill_item_id = request.items[-1].itemId
+                new_item: BackfilledItemObject = BackfilledItemObject(itemId=backfill_item_id, index=item.index)
+                new_items.append(new_item)
+        response: BackfillResponse = BackfillResponse(backfilledItems=new_items)
         self.log_payload(f'{self.Backfill.__name__} response: %s', response)
         return response
-    
-    def log_payload(self, format : str, payload):
+
+    # noinspection PyShadowingBuiltins
+    def log_payload(self, format: str, payload):
         if not self.logger:
             return
         payload_dict = MessageToDict(payload, preserving_proto_field_name=True)
         payload_json = json.dumps(payload_dict)
         self.logger.info(format % payload_json)
-    
